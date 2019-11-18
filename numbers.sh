8b543fc3f07f13f1d6bc77f27c97fe9c803b8a23
“Venezia <vquezada@chapman.edu>
2019-11-17 19:44:05 -0800

    Completed numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# numbers.sh
+# Venezia Quezada
+
+echo "Enter a positive number: "
+read NUMBER
+N=1
+for i in $(seq 1 "$NUMBER")
+do
+        if [ $((N%2)) -eq 0 ]
+        then
+                echo "$i EVEN"
+        else
+                echo "$i ODD"
+        fi
+        N=$(($N+1))
+done
